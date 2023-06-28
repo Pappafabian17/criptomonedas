d7316361fe98b254b6f6ca3ffe3c03d4b0e9f359
Fabian Pappa <fabian.pappa.ext@errepar.com>
2023-06-28 13:08:39 -0300

    proyecto finalizado

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import useSelectMonedas from "../hooks/useSelectMonedas";
+import Error from "./Error";
 import styled from "@emotion/styled";
 import { monedas } from "../data/monedas";
 
@@ -21,17 +22,62 @@ const InputSubmit = styled.input`
     cursor: pointer;
   }
 `;
-const Formulario = () => {
+const Formulario = ({setMonedas}) => {
+  const [criptos, setCriptos] = useState([]);
+  const [error, setError] = useState(false);
+
   const [moneda, SelectMonedas] = useSelectMonedas("Elige tu moneda", monedas);
+  const [criptomoneda, SelectCriptomoneda] = useSelectMonedas(
+    "Elige tu Criptomoneda",
+    criptos
+  );
+
+  useEffect(() => {
+    const consultarApi = async () => {
+      const url =
+        "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD";
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      const arrayCriptos = resultado.Data.map((cripto) => {
+        const objeto = {
+          id: cripto.CoinInfo.Name,
+          nombre: cripto.CoinInfo.FullName,
+        };
+
+        return objeto;
+      });
+
+      setCriptos(arrayCriptos);
+    };
+    consultarApi();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if ([moneda, criptomoneda].includes("")) {
+      setError(true);
+      return;
+    }
+    setError(false)
+    setMonedas({
+      moneda,
+      criptomoneda
+    })
+
+  };
 
   return (
-    <form>
-      <SelectMonedas />
+    <>
+      {error && <Error>Todos los campos son obligatorios</Error>}
+      <form onSubmit={handleSubmit}>
+        <SelectMonedas />
+        <SelectCriptomoneda />
 
-      {state}
-
-      <InputSubmit type="submit" value="Cotizar" />
-    </form>
+        <InputSubmit type="submit" value="Cotizar" />
+      </form>
+    </>
   );
 };
 
